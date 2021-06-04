ab8956f9e34eb7bcaffe94073d4d9f0ce7d01aac
mariah233 <1005131042@qq.com>
2021-06-04 21:39:41 +0800

    change

---
+++ app/src/main/java/com/example/mywebdemo/event/slideEvent.java
@@ -0,0 +1,29 @@
+package com.example.mywebdemo.event;
+
+//viewpager缩小后点击  viewpager控件的左边或者右边 发出的事件
+public class slideEvent extends baseEvent {
+
+    private int type;  //事件类型：点击 ACTION_DOWN ，滑动 ACTION_MOVE
+    private String direction;//点击位置 或者 滑动方向
+
+    public slideEvent(int type, String direction) {
+        this.type = type;
+        this.direction = direction;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+}
---
+++ app/src/main/java/com/example/mywebdemo/fragment/fragAdapter.java
@@ -0,0 +1,74 @@
+package com.example.mywebdemo.fragment;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.example.mywebdemo.constance.fragConst;
+
+public class fragAdapter extends FragmentStatePagerAdapter {
+    private Context context;
+    private FragmentManager fm;
+    public fragAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        this.context = context;
+        this.fm=fm;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragConst.fraglist.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        // Logger.v("fragConst.fraglist.size() "+fragConst.fraglist.size());
+        return fragConst.fraglist.size();
+    }
+
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        return super.instantiateItem(container, position);
+    }
+
+
+    @Override
+    public int getItemPosition(Object object) {
+//        try {
+//            Method method =  object.getClass().getMethod(("getFragTag"),new Class[0]    );
+//            String tag= (String) method.invoke(object,new Object[0]);
+//            if( isDel ){
+//                if( tag.equals(""+id  )  ){
+//                    Logger.d("删除项tag=   "+tag+ "   id=  " +  id );
+//                    return POSITION_NONE;
+//                }else {
+//                    return POSITION_UNCHANGED;
+//                }
+//            }else {
+//                return POSITION_UNCHANGED;
+//            }
+//        } catch (NoSuchMethodException e) {
+//            e.printStackTrace();
+//        } catch (InvocationTargetException e) {
+//            Logger.d("getItemPosition  InvocationTargetException"  );
+//            e.printStackTrace();
+//        } catch (IllegalAccessException e) {
+//            Logger.d("getItemPosition  IllegalAccessException"  );
+//            e.printStackTrace();
+//        }
+
+        //需要实现一种机制避免更新所有的fragment
+        return POSITION_NONE;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        super.destroyItem(container, position, object);
+
+    }
+}
