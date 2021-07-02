dae6b81cb82d7d77b488ed63aeef0570e9bab3b2
Eea <573149657@qq.com>
2021-07-02 10:58:58 +0800

    完成历史记录和书签后端接口连接，用户列表展示“

---
+++ app/src/main/java/com/example/mywebdemo/user/MeActivity.java
@@ -0,0 +1,13 @@
+package com.example.mywebdemo.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
